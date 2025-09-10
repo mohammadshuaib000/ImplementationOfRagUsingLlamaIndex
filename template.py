7b7891f1004b441b02d71e06fb2f233ce57f10b7
mohammadshuaib <mohammadshuaib@windows.windows>
2025-09-10 13:27:02 +0530

    Folder Structure Completed

---
+++ template.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import os
+
+
+list_of_files = [
+    "QAWithPDF/__init__.py",
+    "QAWithPDF/data_ingestion.py",
+    "QAWithPDF/embedding.py",
+    "QAWithPDF/model.api.py",
+    "Experiments/experiments.ipynb",
+    "StreamlitApp.py",
+    "logger.py",
+    "exception.py",
+    "setup.py",
+    "requirements.txt"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+    
+    if (not os.path.exists(filepath) or (os.path.getsize(filepath)) == 0):
+        with open(filepath,"w") as f:
+            pass
